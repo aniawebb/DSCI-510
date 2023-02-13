a9a2fd81fcf08c22b200773c30f1eb905a0a209e
Ania Webb <ania@innovate.ag>
2023-02-13 10:19:31 -0800

    Update lab11_ania_webb.py

--- lab11_ania_webb.py
+++ lab11_ania_webb.py
@@ -16,7 +16,7 @@ def find_weather(location: str) -> float:
     city_to_query = {"q": location}
 
     key_and_host = {
-        "X-RapidAPI-Key": "bfcc4eca1amsh8641a0a32e874fcp180d9ejsn9239b08669b0",
+        "X-RapidAPI-Key": "XXXXXX",
         "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
     }
 
@@ -36,7 +36,7 @@ def find_weather(location: str) -> float:
 
 
 def find_food(location: str) -> list[str, str, str]:
-    api_key = "kepPgXtjyOsfupUPk1DXsc-bgc9COOH5feseYu7nopwieBrUWMv4KQJzpRA-lfjL335IEIWJ0ccgGkrVuboTOju6bw89C-YPQhdWdXur61hwbYOkUVmdn5CYkfFqY3Yx" 
+    api_key = "XXXX" 
     headers = {'Authorization': 'Bearer {}'.format(api_key)}
 
     url = 'https://api.yelp.com/v3/businesses/search'
@@ -64,7 +64,7 @@ def find_food(location: str) -> list[str, str, str]:
 
 
 def find_tweet(username: str) -> list[str, str, str]:
-    api_key = 'AAAAAAAAAAAAAAAAAAAAANxjjAEAAAAAIl4LZVObpv3pKfWjcu9G3XfPBb0%3DZztLzbGOanXJjzuQeUpDO5gy1HlZ53aOxMtZQ8i60jMM1QWSPX'
+    api_key = 'XXXXXX'
     headers = {'Authorization': 'Bearer {}'.format(api_key)}
 
     url = "https://api.twitter.com/2/tweets/search/recent"
